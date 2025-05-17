ba709c55bf3c5f79ef0131d0622b5bfe16d1d301
bryanmsombra27 <bryanjm96@gmail.com>
2025-05-17 14:23:17 -0600

    adding submodules in a dynamic way

--- src/app/pages/modulos/modulos.component.ts
+++ src/app/pages/modulos/modulos.component.ts
@@ -16,11 +16,20 @@ import { ModulosService } from '../../services/modulos.service';
 import { Modulo } from '../../interfaces/modulos.interface';
 import { CommonModule } from '@angular/common';
 import { AccordeonComponent } from '../../components/shared/accordeon/accordeon.component';
+import { TextComponent } from '../../components/shared/inputs/text/text.component';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-modulos',
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ExitoComponent,
     SpinnerComponent,
     BuscadorComponent,
@@ -28,6 +37,7 @@ import { AccordeonComponent } from '../../components/shared/accordeon/accordeon.
     PaginacionComponent,
     CustomModalComponent,
     AccordeonComponent,
+    TextComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './modulos.component.html',
@@ -38,10 +48,42 @@ export default class ModulosComponent implements OnInit {
   customModal!: CustomModalComponent;
   modulos = signal<Modulo[]>([]);
 
+  isSubmodule = signal<boolean>(false);
+  form!: FormGroup;
+
   modulosService = inject(ModulosService);
+  private fb = inject(FormBuilder);
 
   ngOnInit(): void {
+    this.addModuleForm();
     this.getModules();
+  }
+  // AGREGAR MODULO
+  addModuleForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      route: [''],
+      icon: ['', Validators.required],
+      submodules: this.fb.array([]),
+    });
+  }
+  // AGREGAR SUBMODULOS DE FORMA DINAMICA
+  addSubmoduleDynamicForm() {
+    return this.fb.group({
+      name: ['', Validators.required],
+      route: ['', Validators.required],
+      icon: ['', Validators.required],
+    });
+  }
+
+  get submodules(): FormGroup[] {
+    return (this.form.get('submodules') as FormArray).controls as FormGroup[];
+  }
+  addSubmodule() {
+    this.submodules.push(this.addSubmoduleDynamicForm());
+  }
+  removeSubmodule(index: number) {
+    (this.form.get('submodules') as FormArray).removeAt(index);
   }
 
   openModal() {
