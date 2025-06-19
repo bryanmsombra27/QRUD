2a5e4197e0ff07e434ef9ceaac6c569985d06b20
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-19 10:55:07 -0600

    working on responsive

--- src/app/components/shared/btn/btn.component.ts
+++ src/app/components/shared/btn/btn.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn.component.html',
   styleUrl: './btn.component.css',
 })
 export class BtnComponent {
   text = input.required<string>();
+  center = input<boolean>(true);
 }
