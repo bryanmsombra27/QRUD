5813c4fe992722e5076be5bebdfdb279480b4bb3
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-19 20:40:13 -0600

    deploying to render for external testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // url: 'https://nest-projects.onrender.com',
-  url: 'http://localhost:3000',
+  url: ' https://qrud-backend-latest.onrender.com',
+  // url: 'http://localhost:3000',
   secret_key: 'Alejandra Abigail Arduino Hernandez',
   llaveToken: 'token',
   llaveRole: 'rol',
